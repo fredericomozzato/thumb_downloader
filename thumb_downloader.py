7dd93d2a49ac8029e2345323b12ee525c2598f7f
Frederico Mozzato <frederico.smozzato@gmail.com>
2022-12-14 21:00:52 -0300

    Added support to command line arguments.

--- thumb_downloader.py
+++ thumb_downloader.py
@@ -1,12 +1,24 @@
+import os
+from pathlib import Path
 import re
 import requests
+import sys
 import urllib.request
-from pathlib import Path
 
 from bs4 import BeautifulSoup
 
-DEFAULT_PATH = Path("C:\\Users\\fredm\\Downloads")
-URL = "https://www.youtube.com/watch?v=MDJC16ShEDM"
+args = sys.argv
+
+path = Path.home() / "Downloads"
+filename = "thumb_1"
+
+URL = sys.argv[1]
+
+if len(sys.argv) == 4:
+    filename = sys.argv[2] + '.jpg'
+    path = sys.argv[3]
+elif len(sys.argv) == 3:
+    filename = sys.argv[2] + '.jpg'
 
 with urllib.request.urlopen(URL) as response:
     html = response.read()
@@ -18,6 +30,12 @@ for link in soup.find_all('link'):
     if re.search(r"maxresdefault", l):
         res = requests.get(l)
         if res.status_code:
-            with open(DEFAULT_PATH / 'image_2.jpg', 'wb') as img:
-                img.write(res.content)
+            if os.path.exists(path / filename):
+                filename = filename.split('_')
+                new_filename = filename[0] + str(int(filename[1]) + 1)
+                with open(path / (new_filename + '.jpg'), 'wb') as img:
+                    img.write(res.content)
+            else:
+                with open(path / (filename + '.jpg'), 'wb') as img:
+                    img.write(res.content)
         break
